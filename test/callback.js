ff7a4d777a9e83e3b92935d7b242f1d7b616137e
Christian Rädel <christian.raedel@gmail.com>
2015-05-27 12:01:52 +0200

    Call callback on options with default values, too.

--- test/callback.js
+++ test/callback.js
@@ -1,33 +1,48 @@
 var nomnom = require("../nomnom");
 
-exports.testVersion = function(test) {
-   test.expect(1);
-   
-   nomnom().options({
-      date: {
-         callback: function(date) {
-            test.equal(date, "2010-02-03", "date should match value")
-         }
-      }
-   }).parse(["--date=2010-02-03"]);
+exports.testVersion = function (test) {
+    test.expect(1);
 
-   test.done();
-}
+    nomnom().options({
+        date: {
+            callback: function (date) {
+                test.equal(date, "2010-02-03", "date should match value")
+            }
+        }
+    }).parse(["--date=2010-02-03"]);
 
-exports.testReturnString = function(test) {
-   test.expect(1);
+    test.done();
+};
 
-   nomnom().options({
-      version: {
-         flag: true,
-         callback: function() {
-            return "v0.3";
-         }
-      }
-   })
-   .printer(function(string) { 
-      test.equal(0, string.indexOf("v0.3"))
-      test.done();
-   })
-   .parse(["--version"]);
-}
+exports.testReturnString = function (test) {
+    test.expect(1);
+
+    nomnom().options({
+        version: {
+            flag: true,
+            callback: function () {
+                return "v0.3";
+            }
+        }
+    })
+        .printer(function (string) {
+            test.equal(0, string.indexOf("v0.3"));
+            test.done();
+        })
+        .parse(["--version"]);
+};
+
+exports.testDefaultCallback = function (test) {
+    test.expect(1);
+
+    nomnom().options({
+        value: {
+            default: 'test',
+            callback: function (value) {
+                test.equal(value, 'test');
+            }
+        }
+    }).parse([]);
+
+    test.done();
+};
